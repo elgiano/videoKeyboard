41f050f5e57c1811816a4fd22de234a8801f452c
elgiano <elgiano@gmail.com>
2017-10-13 14:45:28 +0200

    hide pointer

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 int main( ){
 	ofSetupOpenGL(1024,768,OF_FULLSCREEN);			// <-------- setup the GL context
     ofSetVerticalSync(true);
+    ofHideCursor();
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
